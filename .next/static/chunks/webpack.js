1059a06487a8180ef6d904f9b608d5584abba14c
Serzhul <wilshere917@gmail.com>
2021-08-30 14:50:09 +0900

    Home button added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "52c72d2720ecdda9d9a4";
+/******/ 	var hotCurrentHash = "4c9d4f46be2d43543d8b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
